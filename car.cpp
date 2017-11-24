4ba5a4be758f325c143c01aec78d6e371b76bfd9
Patrick Spisak <patrick.spisak@gmx.at>
2017-11-24 11:59:00 +0100

    Final status at school

---
+++ car.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "car.h"
+
+bool start(Car car)
+{
+  return false;
+}
+
+void accelerate(Car car)
+{
+
+}
+
+void brake(Car car)
+{
+
+}
+
+int get_speed(Car car)
+{
+  return 0;
+}
+
+float get_filling_level(Car car)
+{
+  return 0;
+}
+
+void fill(Car car, float fuel_amount)
+{
+}
+
+
+void stop(Car car)
+{
+
+}
--- car.h
+++ car.h
@@ -1,10 +1,10 @@
 /*----------------------------------------------------------
-*				HTBLA-Leonding / Klasse: <your class>
+*				HTBLA-Leonding / Klasse: 2AHIF
 * ---------------------------------------------------------
-* Exercise Number: 0
+* Exercise Number: 5
 * Title:			car.h
-* Author:			P. Bauer
-* Due Date:		January 9, 2015
+* Author:			Patrick Spisak
+* Due Date:		November 24 2017
 * ----------------------------------------------------------
 * Description:
 * Car abstract data type demo.
@@ -12,5 +12,24 @@
 */
 #ifndef ___CAR_H
 #define ___CAR_H
+
+enum Colro {RED , GREEN , BLUE , ORANGE , SILVER , BLACK};
+enum CarType {FIAT_MULTIPLA , JEEP , AIXAM};
+
+struct CarType {
+char* type;
+Color color;
+int current_speed;
+};
+
+
+
+bool start(Car car);
+void accelerate(Car car);
+void brake(Car car);
+int get_speed(Car car);
+float get_filling_level(Car car);
+void fill(Car car, float fuel_amount);
+void stop(Car car);
 
 #endif
